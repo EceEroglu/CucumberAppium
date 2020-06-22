003ab55ff12bd6b1bc7542aa1a0076cd0959806a
ece <eceerogluu.89@gmail.com>
2020-06-22 11:21:19 +0300

    feature files updated

--- src/test/java/stepdefs/AddProductStepDef.java
+++ src/test/java/stepdefs/AddProductStepDef.java
@@ -1,7 +1,9 @@
 package stepdefs;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import pages.BabyCategory;
 import pages.Basket;
 import pages.Categories;
@@ -13,138 +15,138 @@ import pages.Toolbar;
 
 public class AddProductStepDef extends BaseTestClass {
 
-	Categories categories = new Categories(driver());
-	Login login = new Login(driver());
-	FoodCategory foodCategory = new FoodCategory(driver());
-	MilkCategory milkCategory = new MilkCategory(driver());
-	BabyCategory babyCategory = new BabyCategory(driver());
-	Toolbar toolbar = new Toolbar(driver());
-	Basket basket = new Basket(driver());
-	Payment payment = new Payment(driver());
+    Categories categories = new Categories(driver());
+    Login login = new Login(driver());
+    FoodCategory foodCategory = new FoodCategory(driver());
+    MilkCategory milkCategory = new MilkCategory(driver());
+    BabyCategory babyCategory = new BabyCategory(driver());
+    Toolbar toolbar = new Toolbar(driver());
+    Basket basket = new Basket(driver());
+    Payment payment = new Payment(driver());
 
-	@Given("^I am logged in$")
-	public void ı_am_logged_in() {
+    @When("^I am logged in$")
+    public void ı_am_logged_in() {
 
-		login.enterUserName("hello@getir.com");
-		login.enterPassword("hello");
-		login.clickLoginButton();
+        login.enterUserName("hello@getir.com");
+        login.enterPassword("hello");
+        login.clickLoginButton();
 
-		categories.isCategoriesPageLoaded();
+        categories.isCategoriesPageLoaded();
 
-	}
+    }
 
-	@Then("^I click on food category$")
-	public void ı_click_on_food_category() {
+    @And("^I click on food category$")
+    public void ı_click_on_food_category() {
 
-		categories.selectFoodCategory();
-		foodCategory.isCategoryDetailPageLoaded();
+        categories.selectFoodCategory();
+        foodCategory.isCategoryDetailPageLoaded();
 
-	}
+    }
 
-	@Then("^I click on add icon for food product$")
-	public void ı_click_on_add_icon_for_food_product() {
+    @And("^I click on add icon for food product$")
+    public void ı_click_on_add_icon_for_food_product() {
 
-		foodCategory.addProduct();
+        foodCategory.addProduct();
 
-	}
+    }
 
-	@Then("^I navigate to back$")
-	public void ı_navigate_to_back() {
+    @And("^I navigate to back$")
+    public void ı_navigate_to_back() {
 
-		foodCategory.navigateBack();
-		categories.isCategoriesPageLoaded();
+        foodCategory.navigateBack();
+        categories.isCategoriesPageLoaded();
 
-	}
+    }
 
-	@Then("^I navigate to milk category$")
-	public void ı_navigate_to_milk_category() {
+    @And("^I navigate to milk category$")
+    public void ı_navigate_to_milk_category() {
 
-		categories.isCategoriesPageLoaded();
-		
+        categories.isCategoriesPageLoaded();
 
-	}
 
-	@Then("^I click on add icon for milk product$")
-	public void ı_click_on_add_icon_for_milk_product() {
+    }
 
-		milkCategory.addProduct();
+    @And("^I click on add icon for milk product$")
+    public void ı_click_on_add_icon_for_milk_product() {
 
-	}
+        milkCategory.addProduct();
 
-	@Then("^I scroll down for baby category$")
-	public void ı_scroll_down_for_baby_category() {
+    }
 
-		categories.scrollSelectBabyCategory();
+    @And("^I scroll down for baby category$")
+    public void ı_scroll_down_for_baby_category() {
 
-	}
+        categories.scrollSelectBabyCategory();
 
-	@Then("^I navigate to baby category detail page$")
-	public void ı_navigate_to_baby_category_detail_page() {
+    }
 
-		babyCategory.isCategoryDetailPageLoaded();
+    @And("^I navigate to baby category detail page$")
+    public void ı_navigate_to_baby_category_detail_page() {
 
-	}
+        babyCategory.isCategoryDetailPageLoaded();
 
-	@Then("^I click on add icon for baby product$")
-	public void ı_click_on_add_icon_for_baby_product() {
+    }
 
-		babyCategory.addProduct();
+    @And("^I click on add icon for baby product$")
+    public void ı_click_on_add_icon_for_baby_product() {
 
-	}
+        babyCategory.addProduct();
 
-	@Then("^I click on basket icon from tool menu$")
-	public void ı_click_on_basket_icon_from_tool_menu() {
+    }
 
-		toolbar.clickBasketIcon();
+    @And("^I click on basket icon from tool menu$")
+    public void ı_click_on_basket_icon_from_tool_menu() {
 
-	}
+        toolbar.clickBasketIcon();
 
-	@Then("^I verify total amount of my products$")
-	public void ı_verify_total_amount_of_my_products() {
+    }
 
-		basket.isBasketPageLoaded();
-		basket.verifyTotalAmount();
+    @Then("^I verify total amount of my products$")
+    public void ı_verify_total_amount_of_my_products() {
 
-	}
+        basket.isBasketPageLoaded();
+        basket.verifyTotalAmount();
 
-	@Then("^I delete my products$")
-	public void ı_delete_my_products() {
+    }
 
-		basket.deleteProduct();
+    @And("^I delete my products$")
+    public void ı_delete_my_products() {
 
-	}
+        basket.deleteProduct();
 
-	@Then("^I verify \"([^\"]*)\" of my products$")
-	public void i_verify_of_my_products(String string) {
+    }
 
-	}
+    @Then("^I verify \"([^\"]*)\" of my products$")
+    public void i_verify_of_my_products(String string) {
 
-	@Then("^I add one more food product$")
-	public void ı_add_one_more_food_product() {
+    }
 
-	}
+    @And("^I add one more food product$")
+    public void ı_add_one_more_food_product() {
 
-	@Then("^I add one more milk product$")
-	public void ı_add_one_more_milk_product() {
+    }
 
-	}
+    @And("^I add one more milk product$")
+    public void ı_add_one_more_milk_product() {
 
-	@Then("^I add one more baby product$")
-	public void ı_add_one_more_baby_product() {
+    }
 
-	}
+    @And("^I add one more baby product$")
+    public void ı_add_one_more_baby_product() {
 
-	@Then("^I navigate to payment page$")
-	public void ı_navigate_to_payment_page() {
+    }
 
-		basket.navigateBack();
-		payment.isPaymentPageLoaded();
+    @And("^I navigate to payment page$")
+    public void ı_navigate_to_payment_page() {
 
-	}
+        basket.navigateBack();
+        payment.isPaymentPageLoaded();
 
-	@Then("^I verify \"([^\"]*)\" of my products from payment page$")
-	public void ı_verify_of_my_products_from_payment_page(String string) {
+    }
 
-	}
+    @Then("^I verify \"([^\"]*)\" of my products from payment page$")
+    public void ı_verify_of_my_products_from_payment_page(String string) {
+
+    }
 
 }
--- src/test/resources/Features/AddProduct.feature
+++ src/test/resources/Features/AddProduct.feature
@@ -2,36 +2,36 @@ Feature: Add product scenarios
 
   Scenario Outline: Validate products in basket
     Given I am on login screen
-    And I am logged in
-    Then I click on food category
-    Then I click on add icon for food product
+    When I am logged in
+    And I click on food category
+    And I click on add icon for food product
     And I navigate to back
-    Then I navigate to milk category
+    And I navigate to milk category
     And I click on add icon for milk product
     And I navigate to back
     And I scroll down for baby category
-    Then I navigate to baby category detail page
+    And I navigate to baby category detail page
     And I click on add icon for baby product
     And I click on basket icon from tool menu
     Then I verify total amount of my products
     And I delete my products
 
-    Examples: 
+    Examples:
       | total amount |
-      |          171 |
+      | 171          |
 
   @E2E
   Scenario Outline: Validate add more products in basket
     Given I am on login screen
     And I am logged in
-    Then I click on food category
-    Then I click on add icon for food product
+    And I click on food category
+    And I click on add icon for food product
     And I navigate to back
-    Then I navigate to milk category
+    And I navigate to milk category
     And I click on add icon for milk product
     And I navigate to back
     And I scroll down for baby category
-    Then I navigate to baby category detail page
+    And I navigate to baby category detail page
     And I click on add icon for baby product
     And I click on basket icon from tool menu
     Then I verify "<total amount>" of my products
@@ -44,6 +44,6 @@ Feature: Add product scenarios
     And I navigate to back
     And I delete my products
 
-    Examples: 
+    Examples:
       | added total amount |
-      |                342 |
+      | 342                |
